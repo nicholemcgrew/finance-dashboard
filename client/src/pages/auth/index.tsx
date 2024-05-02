0b4f37ae745576117b6982c35c3daee039ec8f19
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-02 14:08:47 -0500

    updated server

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,20 +1,23 @@
 import {
-    SignedIn, 
-    SignedOut, 
-    SignInButton, 
-    SignUpButton, 
-    UserButton } from "@clerk/clerk-react";
-
-export const Auth = () => {
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    SignUpButton,
+  } from "@clerk/clerk-react";
+  import { Navigate } from "react-router-dom";
+  
+  export const Auth = () => {
     return (
-        <div className="sign-in-container">
-            <SignedOut>
-                <SignUpButton mode="modal" />
-                <SignInButton mode="modal" />
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-        </div>
-        )
-};
+      <div className="sign-in-container">
+        <SignedOut>
+          <h1> Welcome to Your Own Personal Finance Tracker!</h1>
+          <SignUpButton mode="modal" />
+          <SignInButton mode="modal" />
+        </SignedOut>
+        <SignedIn>
+          <Navigate to="/" />
+        </SignedIn>
+      </div>
+    );
+  };
+  
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,31 @@
-// PWD : xIcFBIzkJ259Dnmd
-import express, { Express } from 'express'
-import mongoose from 'mongoose'
-import financialRecordRouter from './routes/financial-records'
-import cors from 'cors'
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env
+console.log("MongoDB URI:", process.env.MONGODB_URI);
 
-const app: Express = express()
-const port = process.env.PORT || 3001
 
-app.use(express.json())
-app.use(cors())
+const app: Express = express();
+const port = process.env.PORT || 3001;
+const mongoURI = process.env.MONGODB_URI; // Access MongoDB URI from environment variables
 
-const mongoURI: string = 
+if (!mongoURI) {
+  console.error("MongoDB URI is not provided in the environment variables.");
+  process.exit(1); // Exit the process if MongoDB URI is not provided
+}
+
+app.use(express.json());
+app.use(cors());
 
 mongoose
-.connect(mongoURI)
-.then(() => console.log("Connected to DB"))
-.catch((err) => console.error("Failed to connect to DB"))
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
 
-app.use("/financial-records", financialRecordRouter)
+app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
-    console.log(`Server Running on Port ${port}`)
-})
+  console.log(`Server Running on Port ${port}`);
+});
